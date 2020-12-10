b89a8fcfd25e83ade7c1556cd7813861f00e44a6
Le Anh Nhan <lenhanasus4@gmail.com>
2020-12-10 07:06:28 +0700

    remove redundant code

--- Application/Query/Activities/GetAllQuery.cs
+++ Application/Query/Activities/GetAllQuery.cs
@@ -17,20 +17,13 @@ namespace Application.Query.Activities
         public GetAllActivityQueryHandler(ApplicationDbContext context)
         {
             this._context = context;
-            // comment 3
-            //Comenet 9
         }
 
         public async Task<IReadOnlyCollection<Activity>> Handle(GetAllActivityQueryRequest request, CancellationToken cancellationToken)
         {
-            // Comment 1
-            // Coment 7
             var response = await _context.Activities.ToListAsync(cancellationToken);
 
-            return response;
-            // Comment 8
-        
-            // comment 2        
+            return response;      
         }
     }
 }
