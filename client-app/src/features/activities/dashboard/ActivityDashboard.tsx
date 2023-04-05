e10fd17075568a36336d407d8b0e539abba6300c
Nhan Le <lenhanasus4@gmail.com>
2023-04-05 20:01:45 +0700

    #re-build layout app component

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,18 +1,22 @@
 import { observer } from 'mobx-react-lite';
 import { Grid, GridColumn } from 'semantic-ui-react';
 import ActivityList from '../list/ActivityList';
+import NavBar from '../../../components/nav/NavBar';
 
 const ActivityDashboard = () => {
 
   return (
-    <Grid>
-      <GridColumn width={10}>
-        <ActivityList />
-      </GridColumn>
-      <GridColumn width={6}>
-        <h2>Activity Filters</h2>
-      </GridColumn>
-    </Grid>
+    <div className='activity-dashboard'>
+      <NavBar></NavBar>
+      <Grid>
+        <GridColumn width={10}>
+          <ActivityList />
+        </GridColumn>
+        <GridColumn width={6}>
+          <h2>Activity Filters</h2>
+        </GridColumn>
+      </Grid>
+    </div>
   );
 };
 
