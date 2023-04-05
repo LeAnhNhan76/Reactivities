331116058aae7ad10b8c55e4b9a6b4b4748a034f
Nhan Le <nla@onix.com>
2023-04-05 17:30:20 +0700

    #update setup routes

--- client-app/src/layout/App.tsx
+++ client-app/src/layout/App.tsx
@@ -1,20 +1,12 @@
 import { observer } from 'mobx-react-lite';
 import { Outlet } from 'react-router-dom';
 import Spinner from '../components/spinner/Spinner';
-import { useState } from 'react';
-import NavBar from '../features/nav/NavBar';
 
 const App = () => {
-    const [isLoggedIn, setLoggedIn] = useState(true);
-
     return (
         <div className='root-container'>
-            {isLoggedIn && <NavBar></NavBar>}
-            {isLoggedIn && <div className='main-body'>
-                <Outlet></Outlet>
-              </div>}
-            {!isLoggedIn && <Outlet></Outlet>}
-            <Spinner />
+          <Outlet></Outlet>
+          <Spinner></Spinner>
         </div>
     );
 }
