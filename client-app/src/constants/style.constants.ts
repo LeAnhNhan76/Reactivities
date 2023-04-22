d977d63ad80f8094eada242a1cc86f572e85c40c
Nhan Le <lenhanasus4@gmail.com>
2023-04-22 13:58:15 +0700

    #client-app: complete ui main-info section

---
+++ client-app/src/constants/style.constants.ts
@@ -0,0 +1,8 @@
+export const colors = {
+    $white: '#ffffff',
+    $black: '#000000',
+    $teal: '#00b5ad',
+    $lightGray: '#dcdcdc',
+    $backgroundGradientPrimary: 'linear-gradient( 135deg, rgb(24, 42, 115) 0%,rgb(33, 138, 174) 69%,rgb(32, 167, 172) 89%',
+    $backgroundLightPrimary: 'rgb(234, 234, 234)'
+}
--- client-app/src/features/activities/details/main-info/ActivityMainInfo.tsx
+++ client-app/src/features/activities/details/main-info/ActivityMainInfo.tsx
@@ -1,16 +1,16 @@
-import { useParams } from "react-router-dom";
-import { useActivityStore } from "../../../../stores/store";
-import { useEffect } from "react";
-import { Button, Card, Image } from "semantic-ui-react";
-import { formatDate } from "../../../../utils/dateTime.utils";
 import { observer } from "mobx-react-lite";
+import { useEffect } from "react";
+import { Form, useParams } from "react-router-dom";
+import { Button, Card, CardContent, CardHeader, Header, Image, List, ListContent, ListIcon, ListItem, TextArea } from "semantic-ui-react";
+import { useActivityStore } from "../../../../stores/store";
+import { formatDate } from "../../../../utils/dateTime.utils";
+import './index.scss';
+import { colors } from "../../../../constants/style.constants";
 
 const ActivityMainInfo = () => {
     const {
         selectedActivity: activity,
-        setEditMode,
         loadActivity,
-        cancelSelectedActivity
       } = useActivityStore();
     
       const {id} = useParams();
@@ -23,13 +23,9 @@ const ActivityMainInfo = () => {
 
   return (
     <div className="activity-main-info">
-        <Card fluid>
-            <Card.Content>
-                <Image
-                    src={`/assets/categoryImages/${activity?.category}.jpg`}
-                    wrapped
-                    ui={false}
-                />
+        <Card fluid className="banner">
+            <Image src={`/assets/categoryImages/${activity?.category}.jpg`}></Image>
+            <Card.Content className="banner-info">
                 <Card.Header>{activity?.title}</Card.Header>
                 <Card.Meta>
                     <span>Date</span>
@@ -39,18 +35,55 @@ const ActivityMainInfo = () => {
             </Card.Content>
             <Card.Content extra>
                 <Button
-                    onClick={() => setEditMode(true)}
                     basic
-                    color="blue"
-                    content="Edit"
+                    color="orange"
+                    content="Cancel Activity"
                 />
                 <Button
-                    onClick={() => cancelSelectedActivity()}
-                    basic
-                    color="grey"
-                    content="Cancel"
+                    color="orange"
+                    content="Manage Event"
+                    floated="right"
                 />
             </Card.Content>
+        </Card>
+        <Card fluid className="main-info">
+            <List divided relaxed >
+                <ListItem>
+                    <ListIcon name="info" color="teal" verticalAlign="middle"></ListIcon>
+                    <ListContent verticalAlign="middle">
+                        Activity in 5 months in future
+                    </ListContent>
+                </ListItem>
+                <ListItem>
+                    <ListIcon name="calendar" color="teal" verticalAlign="middle"></ListIcon>
+                    <ListContent verticalAlign="middle">
+                        {activity?.date}
+                    </ListContent>
+                </ListItem>
+                <ListItem>
+                    <ListIcon name="map marker" color="teal" verticalAlign="middle"></ListIcon>
+                    <ListContent verticalAlign="middle">
+                        {activity?.city}
+                    </ListContent>
+                </ListItem>
+            </List>
+        </Card>
+        <Card className="chat" fluid>
+            <CardHeader>
+                <Header as={'h3'} block inverted style={{
+                    textAlign: 'center',
+                    color: colors.$white,
+                    backgroundColor: colors.$teal
+                }}>
+                    Chat about this event
+                </Header>
+            </CardHeader>
+            <CardContent>
+                <Form>
+                    <TextArea placeholder='Enter your comment (Enter to submit, SHIFT + enter to new line)' fluid>
+                    </TextArea>
+                </Form>
+            </CardContent>
         </Card>
     </div>
   )
