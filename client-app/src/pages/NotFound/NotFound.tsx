e50d85bb832550a79225695c48a9f18cc7d79ce2
Nhan Le <lenhanasus4@gmail.com>
2023-09-16 14:09:02 +0700

    add 404 page

--- client-app/src/pages/NotFound/NotFound.tsx
+++ client-app/src/pages/NotFound/NotFound.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import { Button, Container, Header, Message } from "semantic-ui-react";
+import "./NotFound.scss";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
-  return <div>Not Found</div>;
+  return (
+    <Container textAlign="center" className="not-found-container">
+      <Header
+        content={"404 - Not found"}
+        subheader={"The page was not found or removed!"}
+        className="text"
+      />
+      <Link to={""}>
+        <Button content="Go back Home" />
+      </Link>
+    </Container>
+  );
 };
 
 export default NotFound;
