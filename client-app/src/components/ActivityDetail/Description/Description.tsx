467f9627235c85544bb574d77dbaa4e5be1c45f4
Nhan Le <lenhanasus4@gmail.com>
2023-09-22 07:20:41 +0700

    add activity detail desc

--- client-app/src/components/ActivityDetail/Description/Description.tsx
+++ client-app/src/components/ActivityDetail/Description/Description.tsx
@@ -1,5 +1,24 @@
+import { Icon, Segment } from "semantic-ui-react";
+import { formatDate } from "../../../utils/dateTime.util";
+import "./Description.scss";
+
 const Description = () => {
-  return <div>Description</div>;
+  return (
+    <Segment.Group className="activity-detail-desc">
+      <Segment>
+        <Icon name="info" color="orange"></Icon>
+        <span>Activity 5 month in future</span>
+      </Segment>
+      <Segment>
+        <Icon name="calendar" color="orange"></Icon>
+        <span>{formatDate(new Date())}</span>
+      </Segment>
+      <Segment>
+        <Icon name="map marker" color="orange"></Icon>
+        <span>Hai Ba Trung Str, Ho Chi Minh City</span>
+      </Segment>
+    </Segment.Group>
+  );
 };
 
 export default Description;
