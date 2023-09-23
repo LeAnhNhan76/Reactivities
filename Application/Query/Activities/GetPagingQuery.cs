16f7009d54f0f32b43189bd6f10cc916e3b8137b
Nhan Le <lenhanasus4@gmail.com>
2023-09-23 16:32:01 +0700

    complete api paging activities list

--- Application/Query/Activities/GetPagingQuery.cs
+++ Application/Query/Activities/GetPagingQuery.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using FrameworkCore.CustomModels;
 using FrameworkCore.Extensions;
 using MediatR;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
@@ -34,15 +35,34 @@ namespace Application.Query
     public class GetPagingActivitiesHandler : IRequestHandler<GetPagingActivitiesRequest, PagedList<ActivityPagingItem>>
     {
         private readonly ApplicationDbContext _context;
+        private Guid _currentUserId { get; set; }
 
-        public GetPagingActivitiesHandler(ApplicationDbContext context)
+        public GetPagingActivitiesHandler(ApplicationDbContext context, IHttpContextAccessor httpContextAccessor)
         {
             _context = context;
+            _currentUserId = httpContextAccessor.HttpContext.CurrentUserId();
         }
 
         public async Task<PagedList<ActivityPagingItem>> Handle(GetPagingActivitiesRequest request, CancellationToken cancellationToken)
         {
-            var query = _context.Activities;
+            var query = _context.Activities.AsQueryable();
+
+            if (!string.IsNullOrEmpty(request.SearchText))
+            {
+                query = query.Where(x => x.Title.Contains(request.SearchText) ||
+                     x.Description.Contains(request.SearchText) ||
+                     x.Category.Contains(request.SearchText)).AsQueryable();
+            }
+
+            if (!string.IsNullOrEmpty(request.Category))
+            {
+                query = query.Where(x => x.Category == request.Category);
+            }
+
+            if (request.IsHosting)
+            {
+                query = query.Where(x => x.HostId == _currentUserId);
+            }
 
             var totalItems = await query.CountAsync();
             var items = await query.Paging(request.PageIndex, request.ItemsPerPage)
--- FrameworkCore/Extensions/HttpContextExt.cs
+++ FrameworkCore/Extensions/HttpContextExt.cs
@@ -1,12 +1,26 @@
+using System;
+using System.Linq;
+using FrameworkCore.Constants;
 using Microsoft.AspNetCore.Http;
 
 namespace FrameworkCore.Extensions
 {
     public static class HttpContextExt
     {
-        public static string GetFullHostServer (this HttpRequest request) 
+        public static string GetFullHostServer(this HttpRequest request)
         {
             return string.Concat(request.Scheme, "://", request.Host, request.PathBase);
+        }
+
+        public static Guid CurrentUserId(this HttpContext context)
+        {
+            if (context == null) return Guid.Empty;
+
+            var userId = context?.User.Claims.FirstOrDefault(x => x.Type == ReactivitiesClaimTypes.CurrentUserId);
+
+            if (userId == null) return Guid.Empty;
+
+            return new Guid(userId.Value);
         }
     }
 }
